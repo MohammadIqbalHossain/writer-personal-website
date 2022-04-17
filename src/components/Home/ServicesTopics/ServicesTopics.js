c62a9a36d7431350735ad0aae158d8dc6a0608f1
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-04-17 13:23:13 +0600

    Icons added

--- src/components/Home/ServicesTopics/ServicesTopics.js
+++ src/components/Home/ServicesTopics/ServicesTopics.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { BsJournalAlbum } from 'react-icons/bs'
+import { IoDocumentAttach } from 'react-icons/io5'
+import { SiPurescript } from 'react-icons/si'
+import { HiDocumentReport } from 'react-icons/hi'
 const ServicesTopics = () => {
     return (
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-10 mx-10">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-10 mt-10 bg-gray-100">
             <div className="flex flex-col items-center my-10 ">
                 <div className="text-[50px] bg-[#60ECB6] p-5">
                     <BsJournalAlbum />
@@ -13,25 +16,25 @@ const ServicesTopics = () => {
 
             <div className="flex flex-col items-center my-10 ">
                 <div className="text-[50px] bg-[#60ECB6] p-5">
-                    <BsJournalAlbum />
+                    <HiDocumentReport />
                 </div>
-                <h1 className="text-xl mt-5 font-bold">Life Journal</h1>
+                <h1 className="text-xl mt-5 font-bold">Case Studies</h1>
                 <p className="text-sm text-gray-400 font-light font-serif tracking-widest">Lorem ipsum dolor sit amet consectetur</p>
             </div>
 
             <div className="flex flex-col items-center my-10 ">
                 <div className="text-[50px] bg-[#60ECB6] p-5">
-                    <BsJournalAlbum />
+                    <SiPurescript />
                 </div>
-                <h1 className="text-xl mt-5 font-bold">Life Journal</h1>
+                <h1 className="text-xl mt-5 font-bold">Script Writing</h1>
                 <p className="text-sm text-gray-400 font-light font-serif tracking-widest">Lorem ipsum dolor sit amet consectetur</p>
             </div>
 
             <div className="flex flex-col items-center my-10 ">
                 <div className="text-[50px] bg-[#60ECB6] p-5">
-                    <BsJournalAlbum />
+                    <IoDocumentAttach />
                 </div>
-                <h1 className="text-xl mt-5 font-bold">Life Journal</h1>
+                <h1 className="text-xl mt-5 font-bold">Documentations</h1>
                 <p className="text-sm text-gray-400 font-light font-serif tracking-widest">Lorem ipsum dolor sit amet consectetur</p>
             </div>
            
